a74509484d28fa6e4c3a3d5db158434dd17f42d1
langbai23 <517527504@qq.COM>
2025-10-06 10:16:25 -0700

    dhdsjx

---
+++ Assets/Scripts/Mainmenu/Startcontrol.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DebtJam
+{
+    public class StartMenuController : MonoBehaviour
+    {
+        [Header("UI References")]
+        public Button startButton;
+        public AudioSource clickSound;
+
+        void Start()
+        {
+            if (startButton)
+            {
+                startButton.interactable = false; // 启动时暂时禁用按钮
+                Invoke(nameof(EnableButton), 0.1f); // 半秒后启用按钮，避免误触
+            }
+        }
+
+        void EnableButton()
+        {
+            if (startButton)
+            {
+                startButton.interactable = true;
+                startButton.onClick.AddListener(OnStartClicked);
+            }
+        }
+
+        void OnStartClicked()
+        {
+            if (clickSound)
+                clickSound.Play();
+
+            SceneManager.LoadScene("Start_Scene");
+        }
+    }
+}
